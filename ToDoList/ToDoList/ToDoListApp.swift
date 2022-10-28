d6539135b298576b76da92b988c134b8d4fc5dd0
mingxianwei <mingxianwei007@gmail.com>
2022-10-28 14:15:46 +0800

    初始化 创建工程

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 明先伟 on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
